2869c4d7dd6c57347ff3801d56e7bc5622e84dbf
vinhhv <vinhzhz@gmail.com>
2016-06-09 13:55:00 +0700

    Initial Commit

---
+++ training-batch2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  training-batch2
+//
+//  Created by VinhHV on 6/9/16.
+//  Copyright © 2016 VinhHV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
